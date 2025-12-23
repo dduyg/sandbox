a9bf37eee2bb6c3f4cce0232b47ff37a13289ed0
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2025-12-23 12:47:14 +0100

    Create 66.js

---
+++ 66.js
@@ -0,0 +1,49 @@
+from @shelf/06/BLOBs/blob-05.png to @shelf
+     @shelf/06/BLOBs/blob-06.png to @shelf
+     @shelf/06/BLOBs/blob-07.png to @shelf
+     @shelf/06/BLOBs/blob-08.png to @shelf
+     @shelf/06/BLOBs/blob-10.png to @shelf
+     @shelf/06/BLOBs/blob-16.gif to @shelf
+     @shelf/06/BLOBs/blob-35.png to @shelf
+     @shelf/06/DolphinDatabase.png to @shelf
+     @shelf/06/GeometricAir.jpg to @shelf
+     @shelf/06/MountFujiLoading.png to @shelf
+     @shelf/06/NET-NEUTRALITY.gif to @shelf
+     @shelf/06/VHScloud.png to @shelf
+     @shelf/06/audacity.gif to @shelf
+     @shelf/06/backed.jpg to @shelf
+     @shelf/06/detail-12.png to @shelf
+     @shelf/06/detail-13.gif to @shelf
+     @shelf/06/detail-18.png to @shelf
+     @shelf/06/detail-4.jpg to @shelf
+      "https://cdn.jsdelivr.net/gh/dduyg/LiminalLoop@shelf/06/glidingIntegers.gif",
+      "https://cdn.jsdelivr.net/gh/dduyg/LiminalLoop@shelf/06/grayscale-wave.png",
+      "https://cdn.jsdelivr.net/gh/dduyg/LiminalLoop@shelf/06/helloWorld.gif",
+      "https://cdn.jsdelivr.net/gh/dduyg/LiminalLoop@shelf/06/inwave1759.png",
+      "https://cdn.jsdelivr.net/gh/dduyg/LiminalLoop@shelf/06/inwave1761.png",
+      "https://cdn.jsdelivr.net/gh/dduyg/LiminalLoop@shelf/06/inwave1779.png",
+      "https://cdn.jsdelivr.net/gh/dduyg/LiminalLoop@shelf/06/kemalist-devrim.jpg",
+      "https://cdn.jsdelivr.net/gh/dduyg/LiminalLoop@shelf/06/morphology.jpg",
+      "https://cdn.jsdelivr.net/gh/dduyg/LiminalLoop@shelf/06/partOfYou1.png",
+      "https://cdn.jsdelivr.net/gh/dduyg/LiminalLoop@shelf/06/partOfYou2.png",
+      "https://cdn.jsdelivr.net/gh/dduyg/LiminalLoop@shelf/06/popeye.png",
+      "https://cdn.jsdelivr.net/gh/dduyg/LiminalLoop@shelf/06/register-4.jpg",
+      "https://cdn.jsdelivr.net/gh/dduyg/LiminalLoop@shelf/06/satisfyingSetting.mp4",
+      "https://cdn.jsdelivr.net/gh/dduyg/LiminalLoop@shelf/06/sharkTeeth.png",
+      "https://cdn.jsdelivr.net/gh/dduyg/LiminalLoop@shelf/06/signoftheTimes.png",
+      "https://cdn.jsdelivr.net/gh/dduyg/LiminalLoop@shelf/06/spendthrift.gif",
+      "https://cdn.jsdelivr.net/gh/dduyg/LiminalLoop@shelf/06/splashthick-pink-fluid.png"
+    ],
+    "favicons": [
+      "https://cdn.jsdelivr.net/gh/dduyg/LiminalLoop@main/06/favicons/slate.ico",
+      "https://cdn.jsdelivr.net/gh/dduyg/LiminalLoop@shelf/06/favicons/RGB-cinnamonSatin.ico",
+      "https://cdn.jsdelivr.net/gh/dduyg/LiminalLoop@shelf/06/favicons/RGB-electricredBlush.ico",
+      "https://cdn.jsdelivr.net/gh/dduyg/LiminalLoop@shelf/06/favicons/RGB-greenPeriwinkle.ico",
+      "https://cdn.jsdelivr.net/gh/dduyg/LiminalLoop@shelf/06/favicons/RGB-redPiink.ico",
+      "https://cdn.jsdelivr.net/gh/dduyg/LiminalLoop@shelf/06/favicons/sixArrows.ico"
+    ]
+    "github_raw": [
+      "https://raw.githubusercontent.com/dduyg/LiminalLoop/shelf/06/WaterCity.gif",
+      "https://raw.githubusercontent.com/dduyg/LiminalLoop/shelf/06/kemalizm.mp4"
+      "https://cdn.jsdelivr.net/gh/dduyg/LiminalLoop@shelf/06/satisfyingSetting.mp4"
+    ]
