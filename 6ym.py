152ed3cfb5b612305f810ff7c26fd6585be3ed80
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2025-12-25 03:16:17 +0100

    Update 6ym.py

--- 6ym.py
+++ 6ym.py
@@ -3,7 +3,3 @@
 >> LOCAL UPLOAD :: Select files to process")
 
 >> INPUT SOURCE :: Select data origin")
-
-print("\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
-print("  ▓▓▓ SOURCE REPOSITORY CONFIGURATION")
-print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
