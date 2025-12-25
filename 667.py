9de217cba8629a9003dffa8212ea5b1552d2612f
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2025-12-25 21:51:29 +0100

    Update 667.py

--- 667.py
+++ 667.py
@@ -5,12 +5,26 @@ print(f"      ├── total.input: (total inputted at first to process)")
 print(f"      ├── status.success: (total processed glyphs)")
 print(f"      ├── status.skipped: (total skipped)")
 print(f"      ├── commit.type: (library_init or library_update)")
-print(f"      │   ├── 256 glyphs in total")
-print(f"      │   └── 2 catalogs updated")
+print(f"      │   ├── ((if existing_glyphs) `{len(existing_glyphs)} + {len(all_data)} = {len(all_glyphs)} glyphs in total` (else) `{len(all_data)} glyphs`)")
+print(f"      │   └── ((if existing_glyphs) `2 catalogs updated` (else) `2 catalogs created`)")
 print(f"      ├── input.mode: `SELECT.FROM.LOCAL.COMPUTER` ( if chosen 1) or `FETCH.FROM.REPOSITORY` (if 2)")
 print(f"      └── location: (dest_repo)")
 print(f"        └── [↗] https://github.com/link-to-dest-repo")
 print(f"\n    yp//4/%5tgghatoutput_folder")
+
+
+if skipped:
+            print(f"██   >_  ⟨⚠⟩ {len(skipped)} file(s) skipped:")
+            for msg in skipped:
+                print(f"██        >> {msg}")
+        
+        if existing_glyphs:
+            print(f"██   >_  ⟨◢◣⟩ Ltotal")
+        
+        print("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+            
+
+
 
 
 """
